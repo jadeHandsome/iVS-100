f0521b0505048fc6a6eed0a89319890bc5368340
拿把破菜刀 <1149769831@qq.com>
2018-01-06 20:41:56 +0800

    no message

---
+++ iscanDemo/Class/List/Tree/CLTreeView_LEVEL1_Model.h
@@ -0,0 +1,17 @@
+//
+//  CLTreeView_LEVEL1_Model.h
+//  CLTreeView
+//
+//  Created by 钟由 on 14-9-9.
+//  Copyright (c) 2014年 flywarrior24@163.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CLTreeView_LEVEL1_Model : NSObject
+
+@property (strong,nonatomic) NSString *name;
+@property (strong,nonatomic) NSString *signture;
+@property (strong,nonatomic) NSString *sonCnt;
+@property (assign,nonatomic) BOOL isZhan;
+@end
